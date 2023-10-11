4d164e884dfe2c641abedcd8a8fb98d43cf01bf7
Sebastián Reina Balcázar <sebastian.reina_bal@uao.edu.co>
2023-10-11 08:56:15 -0500

    pushee sin guardar, comit funciones, ruta -> assets/clases js

---
+++ src/assets/clases Js/funciones.js
@@ -0,0 +1,19 @@
+let x = 5;
+function primosNormal(n){
+    if(n%2 == 0){
+        console.log("El numero es primo")
+    }else{
+        console.log("El numero no es primo")
+    }
+}
+primosNormal(x);
+
+const primosFlecha = (n) => {
+    if(n%2 == 0){
+        console.log("El numero es primo")
+    }else{
+        console.log("El numero no es primo")
+    }
+}
+
+primosFlecha(x);
