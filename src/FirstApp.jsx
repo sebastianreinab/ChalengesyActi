e4352aa0ef55cb51f2b489969555fcf4fcfc599a
Sebastián Reina Balcázar <sebastian.reina_bal@uao.edu.co>
2023-11-22 21:59:57 -0500

    aqui metiendome un turbito con estos chalenges hasta el giftgrid que sí me loquea

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,15 +1,30 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import { useState } from "react";
 
+export const FirstApp = ({value = 0}) => {
 
-const title = "First App";
-const text = "11"
+    const [counter,setCounter] = useState(value)
 
-export const FirstApp = () => {
+    const handleAdd =()=> {
+        setCounter(counter + 1)
+    }
+
+    const handleResta =()=> {
+        setCounter(counter - 1 )
+    }
+
+    const handlePrincipio =()=> {
+        setCounter(value)
+    }
 
     return(
         <>
-            <h1> {title} </h1>
-            <span> {text} </span>
+            <h1> Counter </h1>
+            <span> {counter} </span>
+            <button onClick={() => handleAdd()}> +1 </button>
+            <button onClick={() => handleResta()}> -1 </button>
+            <button onClick={() => handlePrincipio()}> Principio </button>
         </>
     )
 }
