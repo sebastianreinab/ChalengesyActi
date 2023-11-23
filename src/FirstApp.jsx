d145906b5a3106a6ad12ec9795abaa76a028d6b6
Sebastián Reina Balcázar <sebastian.reina_bal@uao.edu.co>
2023-11-22 21:47:48 -0500

    chalenge 2 FirstApp

---
+++ src/FirstApp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const title = "First App";
+const text = "11"
+
+export const FirstApp = () => {
+
+    return(
+        <>
+            <h1> {title} </h1>
+            <span> {text} </span>
+        </>
+    )
+}
